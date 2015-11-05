e26057a47d22c66d4dfe858cb57be266972924b1
marco <chez.marco@laposte.net>
2015-11-05 19:44:51 +0100

    - add dropdown menus (scrollable because lot of data) to filter by genres, realisateurs, acteurs and global search - move data collect to application_controller.rb because array variables created are available to pages as PageController inherits from ApplicationController - css: add commented area to control bootstrap screen sizes if needed (for collapse option) and customize a little renderings...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,45 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  before_filter :set_data
+
   def hello
     render text: "hello, world!"
   end
+
+  def self.capitalize_first_letter(s)
+    s.nil? ? s : s.split.map(&:capitalize)*' '
+  end
+
+  def self.treat(a)
+    a.sort!.reject{ |s| s.empty? }
+  end
+
+  private      
+    def set_data
+
+      @allrecords = Record.all
+      @genres = Array.new
+      @acteurs = Array.new
+      @realisateurs = Array.new
+      @nationalites = Array.new
+      @annees = Array.new
+
+      @allrecords.each do |record|
+        @genres += record.genre.nil? ? [] : record.genre.split(",").map{ |s| capitalize_first_letter(s.strip)}
+        @acteurs += record.acteur.nil? ? [] : record.acteur.split(",").map{ |s| capitalize_first_letter(s.strip)}
+        @realisateurs += record.realisateur.nil? ? [] : record.realisateur.split(",").map{ |s| capitalize_first_letter(s.strip)}
+        @nationalites += record.nationalite.nil? ? [] : record.nationalite.split(",").map{ |s| capitalize_first_letter(s.strip)}
+        @annees += record.annee.nil? ? [] : [record.annee]
+      end
+
+      @genres = treat(@genres.uniq.nil? ? @genres : @genres.uniq)
+      @acteurs = treat(@acteurs.uniq.nil? ? @acteurs : @acteurs.uniq)
+      @realisateurs = treat(@realisateurs.uniq.nil? ? @realisateurs : @realisateurs.uniq)
+      @nationalites = treat(@nationalites.uniq.nil? ? @nationalites : @nationalites.uniq)
+    end
+
 
 end
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -11,12 +11,18 @@ class PageController < ApplicationController
   helper_method :capitalize_first_letter, :treat
   
   def home
-  	@records = Record.all
-    @acteurs = Array.new
-    @realisateurs = Array.new
-    @genres = Array.new
-    @nationalites = Array.new
-    @annees = Array.new
+    if params[:genre]
+      @records = Record.where("genre like ?",  "%#{params[:genre]}%")
+    elsif params[:realisateur]
+      @records = Record.where("realisateur like ?",  "%#{params[:realisateur]}%")
+    elsif params[:acteur]
+      @records = Record.where("acteur like ?",  "%#{params[:acteur]}%")
+    elsif params[:search]
+      @records = Record.where("acteur like ? or nom like ? or nomoriginal like ? or realisateur like ? or genre like ? or annee like ? or nationalite like ?",  
+        "%#{params[:search]}%",  "%#{params[:search]}%",  "%#{params[:search]}%",  "%#{params[:search]}%",  "%#{params[:search]}%",  "%#{params[:search]}%", "%#{params[:search]}%")
+    else
+    	@records = Record.all
+    end
   end
 
   def detail
