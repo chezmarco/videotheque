e3a73da1b8881efd7fbcc2ead43a55924391118a
marco <chez.marco@laposte.net>
2015-11-06 09:43:39 +0100

    New test to fix heroku search option doesn't work...

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -12,13 +12,17 @@ class PageController < ApplicationController
   
   def home
     if params[:genre]
-      @records = Record.where("genre like ?",  "%#{params[:genre]}%")
+      @records = Record.where("LOWER(genre) LIKE ?",  "%#{params[:genre].downcase}%")
     elsif params[:realisateur]
-      @records = Record.where("realisateur like ?",  "%#{params[:realisateur]}%")
+      @records = Record.where("LOWER(realisateur) LIKE ?",  "%#{params[:realisateur.downcase]}%")
     elsif params[:acteur]
-      @records = Record.where("acteur like ?",  "%#{params[:acteur]}%")
+      @records = Record.where("LOWER(acteur) LIKE ?",  "%#{params[:acteur.downcase]}%")
     elsif params[:search]
-      @records = Record.where("acteur like ?", "%#{params[:search]}%")
+      @records = Record.where("LOWER(acteur) LIKE ? OR LOWER(nom) LIKE ? OR LOWER(nomoriginal) LIKE ? 
+        OR LOWER(realisateur) LIKE ? OR LOWER(genre) LIKE ? OR LOWER(annee) LIKE ? OR LOWER(nationalite) LIKE ?",  
+        "%#{params[:search].downcase}%",  "%#{params[:search].downcase}%",  "%#{params[:search].downcase}%",  
+        "%#{params[:search].downcase}%",  "%#{params[:search].downcase}%",  "%#{params[:search].downcase}%", 
+        "%#{params[:search].downcase}%")
     else
     	@records = Record.all
     end
