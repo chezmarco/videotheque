dfb05b65b2c9e4cd084cf2f5b773a8b3a90130f8
marco <chez.marco@laposte.net>
2015-11-08 20:10:36 +0100

    To fix pb with dropdown menu when "collapsed can't scroll", I decided to hide other <li> elements of the collapsed menu using javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,19 @@
 //= require bootstrap
 //= require_tree .
 
-$(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
+//$(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
+
+$(function () {
+  $("#navbar li.dropdown").click( function() {
+        if ($(this).hasClass('open')){
+          $("#navbar.navbar-collapse.collapse.in li.linav").show();
+          $(this).css('background-color', '#0099cc !important')
+        }
+        else {
+          $("#navbar.navbar-collapse.collapse.in li.linav").hide();
+          $(this).show();
+        }
+  });
+
+  return false;
+});
