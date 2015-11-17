6b18f7e52901bd664a3d4a428222f7da8ecfd7c9
marco <chez.marco@laposte.net>
2015-11-17 17:01:08 +0100

    test for heroku as renaming paperclip images uploaded doesn't work...

--- app/models/record.rb
+++ app/models/record.rb
@@ -5,8 +5,8 @@ class Record < ActiveRecord::Base
 	#validates :NOM, presence: true
   
   #specify that the img_small is a paperclip file attachment
-  has_attached_file :img_small
-  has_attached_file :img_big
+  has_attached_file :img_small, :path => "app/assets/images/:id.jpg", :url => ":id.jpg"
+  has_attached_file :img_big, :path => "app/assets/images/:id_max.jpg", :url => ":id_max.jpg"
   do_not_validate_attachment_file_type :img_small
   do_not_validate_attachment_file_type :img_big
 
