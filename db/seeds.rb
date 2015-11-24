27cac0fd67d84b9c44a9969553fd6ea335f574d5
marco <chez.marco@laposte.net>
2015-11-24 17:27:00 +0100

    add default admin account: admin@admin.com with password: administrator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT ADMIN LOGIN'
+user = User.create! :email => 'admin@admin.com', :password => 'administrator', :password_confirmation => 'administrator'
+user.toggle!(:admin)
+puts 'New user created: ' << user.email
